12dcf2a6bb76ad38fd63bf610ada029f0829d263
Bablu Roy <you@example.com>
2022-01-13 08:50:15 +0530

    first commit

---
+++ Backend/controllers/paymentb.js
@@ -0,0 +1,40 @@
+const braintree = require("braintree");
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANT_ID,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+});
+
+exports.getToken = () => {
+  gateway.clientToken.generate({}, (err, res) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (err, result) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
---
+++ FrontEnd/src/core/StripeCheckout.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
+import { cartEmpty, loadCart } from "./helper/cartHelper";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
+
+const StripeCheckout = ({
+  products,
+  setReload = (f) => f,
+  reload = undefined,
+}) => {
+  const [data, setData] = useState({
+    loading: false,
+    success: false,
+    error: "",
+    address: "",
+  });
+
+  const token = isAuthenticated() && isAuthenticated().token;
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+
+  const getFinalAmount = () => {
+    let amount = 0;
+    products.map((p) => {
+      amount = amount + p.price;
+    });
+    return amount;
+  };
+
+  const makePayment = (token) => {
+    const body = {
+      token,
+      products,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log(response);
+        const { status } = response;
+        console.log("STATUS ", status);
+
+        const orderData = {
+          products: products,
+          transaction_id: response.id,
+          amount: response.amount,
+        };
+        createOrder(userId, token, orderData);
+        cartEmpty();
+        setReload(!reload);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const showStripeButton = () => {
+    return isAuthenticated() ? (
+      <StripeCheckoutButton
+        stripeKey="pk_test_51Ien0sSASz2mjG14j0r3z4IA6XmaChjx0xAyZwfbcLQw34Obsm25RXSTPMtRy1tMT8ceklWL7sJ5nIT5tdxEcCUv00pKD2SLkY"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Buy Tshirts"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with stripe</button>
+      </StripeCheckoutButton>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-warning">Signin</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h3 className="text-white">Stripe Checkout {getFinalAmount()}</h3>
+      {showStripeButton()}
+    </div>
+  );
+};
+
+export default StripeCheckout;
